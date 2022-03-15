0a2eb03d8a9a44c2966ed9acb9f890931faeb7d0
Ted Liu <mr.liu0518@outlook.com>
2022-03-15 11:51:52 +0800

    Update test.ino

--- test/test.ino
+++ test/test.ino
@@ -1,7 +1,43 @@
+// Multipurpose_Electronic_Scales
+//
+// Arduino_UNO_Rev3 ATmega328P && Arduino_Nano ATmega328 controlled multipurpose electronic scale.
+//
+// This version of the code implements:
+// - Calibrating and storing the scale factor into the EEPROM
+// - Automatically enter measure mode after start-up
+// - Setup menu by long-pressing the Esc button
+// - Two Relay pin 
+// 
+// Power supply should be in the range of 7-12V according to the
+// Arduino_UNO_Rev3 tech specs.
+//
+// For calibration you need a known weight load that close to the 
+// maximum range of your load cell.
+//
+// Board: Arduino UNO Rev3 && Arduino Nano
+// Microcontroller: ATmega328P && ATmega328
+// Clock speed: 16 MHz
+//
+// Contributions & Special thanks:
+// The program structure refers to the following project:
+// - Stefan Wagner, https://github.com/wagiminator/ATmega-Soldering-Station
+// Contributors:
+// - 
+//
+// 2022 by Ted Liu
+// Project Files (Github): https://github.com/tedLiu0518/Arduino_UNO_multipurpose_electronic_scale
+// 
+// License: 
+// - Creative Commons Attribution-ShareAlike 3.0 License (CC BY-SA 3.0)
+// - https://creativecommons.org/licenses/by-sa/3.0/
+//
 // Libraries
 #include "LiquidCrystal_I2C.h"              // https://github.com/johnrickman/LiquidCrystal_I2C
 #include "HX711.h"                          // https://github.com/bogde/HX711
 #include "EEPROM.h"                         // https://github.com/PaulStoffregen/EEPROM
+
+// Firmware version
+#define  VERSION              "v0.4"
 
 // Pins
 #define  HX711_DT_PIN              2        // HX711 module Data IO Connection
@@ -24,10 +60,10 @@
 #define  EEPROM_IDENT         0xE76A        // to identify if EEPROM was written by this program
 
 // Menu items
-const String MainScreenItems[]     =  { "Measure", "Setting", "Information", "Advance" };
-const String StoreItems[]           =  { "Store factor?", "Press ENTER" };
-const String PutSampleItems[]       =  {  "Put sample..", "Press ENTER" };
-const String ClearItems[]          =  {  "Clear..", "Press ENTER" };
+const String MainScreenItems[]     =  { "Measure", "Setting", "Information" };
+const String StoreItems[]          =  { "Store factor?", "Press ENTER" };
+const String PutSampleItems[]      =  { "Put sample..", "Press ENTER" };
+const String ClearItems[]          =  { "Clear..", "Press ENTER" };
 
 // Timing && selection variables
 uint32_t     buttonMillis, currentMillis, lcdMillis;
@@ -60,6 +96,9 @@ void setup() {
     scale.begin(HX711_DT_PIN, HX711_SCK_PIN);
     scale.power_down();	
 
+    // get default values from EEPROM
+    getEEPROM();
+
     Measure();
 }
 
@@ -85,12 +124,12 @@ void updateEEPROM() {
 }
 
 void checkRelay(float value) {
-    if(!digitalRead(BTN_DOWN_PIN)) {
+    if (!digitalRead(BTN_DOWN_PIN)) {
         digitalWrite(RELAY_1_PIN, HIGH);
         digitalWrite(RELAY_2_PIN, LOW);
     }
     else {
-        if(value >= RELAY_VALUE) {
+        if (value >= RELAY_VALUE) {
             digitalWrite(RELAY_1_PIN, HIGH);
         }
         else {
@@ -103,7 +142,7 @@ void checkRelay(float value) {
 }
 
 void ResetRelay() {
-    if(!digitalRead(BTN_UP_PIN) || !digitalRead(BTN_DOWN_PIN)) {}
+    if (!digitalRead(BTN_UP_PIN) || !digitalRead(BTN_DOWN_PIN));
     else {
         digitalWrite(RELAY_2_PIN, HIGH);
         digitalWrite(RELAY_1_PIN, HIGH);
@@ -111,7 +150,7 @@ void ResetRelay() {
 }
 
 void measureScreen(float value) {
-    if(currentMillis - lcdMillis >= REFRESH_RATE) {
+    if (currentMillis - lcdMillis >= REFRESH_RATE) {
         lcd.clear();
         lcd.setCursor(0,0);
         lcd.print(value);
@@ -129,7 +168,7 @@ void Measure() {
     lcd.setCursor(0,0);
     lcd.print("Measuring..");
     lcdMillis = millis();
-    while(!buttonCheck(BTN_ESC_PIN, LONG_PRESS)) {
+    while (!buttonCheck(BTN_ESC_PIN, LONG_PRESS)) {
         float value = scale.get_units(10);
         currentMillis = millis();
         measureScreen(value);
@@ -142,7 +181,7 @@ void Measure() {
 }
 
 void Screen2Items(String Items[]) {
-    if(currentMillis - lcdMillis >= REFRESH_RATE) {
+    if (currentMillis - lcdMillis >= REFRESH_RATE) {
         lcd.clear();
         lcd.setCursor(0,0);
         lcd.print(Items[0]);
@@ -154,24 +193,24 @@ void Screen2Items(String Items[]) {
 
 uint16_t numberInput(int numberSize){
     int selectedNum[numberSize] = {0};
-    for(int i = 0; i <= numberSize-1 ; i++){
+    for (int i = 0; i <= numberSize-1 ; i++) {
         lcdMillis = millis();
-        while(!buttonCheck(BTN_ENTER_PIN, SHORT_PRESS)){
+        while (!buttonCheck(BTN_ENTER_PIN, SHORT_PRESS)) {
             currentMillis = millis();
-            if(currentMillis - lcdMillis >= REFRESH_RATE){
+            if (currentMillis - lcdMillis >= REFRESH_RATE) {
                 CalibrationScreen(selectedNum, numberSize);
                 lcdMillis = millis();
             }
-            if(buttonCheck(BTN_DOWN_PIN, SHORT_PRESS) && (selectedNum[i] != 0)) {
+            if (buttonCheck(BTN_DOWN_PIN, SHORT_PRESS) && (selectedNum[i] != 0)) {
                 selectedNum[i]--;
             }
-            if(buttonCheck(BTN_UP_PIN, SHORT_PRESS) && (selectedNum[i] != 9)) {
+            if (buttonCheck(BTN_UP_PIN, SHORT_PRESS) && (selectedNum[i] != 9)) {
                 selectedNum[i]++;
             }
         }
     }
     uint16_t numberReturn = 0;
-    for(int i = 0; i<= numberSize-1 ; i++){
+    for (int i = 0; i<= numberSize-1 ; i++) {
         numberReturn = numberReturn + (selectedNum[i]*pow(10,i));
     }
     return numberReturn;
@@ -181,13 +220,13 @@ void Calibration() {
     scale.power_up();
     lcd.clear();
     lcdMillis = millis();
-    while(1) {
+    while (1) {
         currentMillis = millis();
         Screen2Items(ClearItems);
-        if(buttonCheck(BTN_ENTER_PIN, SHORT_PRESS)) {
+        if (buttonCheck(BTN_ENTER_PIN, SHORT_PRESS)) {
             break;
         }
-        if(buttonCheck(BTN_ESC_PIN, SHORT_PRESS)) {
+        if (buttonCheck(BTN_ESC_PIN, SHORT_PRESS)) {
             mainScreen();
         }
     }
@@ -196,13 +235,13 @@ void Calibration() {
     float sample_weight = (float)numberInput(5)/100.00;
     lcd.clear();
     lcdMillis = millis();
-    while(1) {
+    while (1) {
         currentMillis = millis();
         Screen2Items(PutSampleItems);
-        if(buttonCheck(BTN_ENTER_PIN, SHORT_PRESS)) {
+        if (buttonCheck(BTN_ENTER_PIN, SHORT_PRESS)) {
             break;
         }
-        if(buttonCheck(BTN_ESC_PIN, SHORT_PRESS)) {
+        if (buttonCheck(BTN_ESC_PIN, SHORT_PRESS)) {
             mainScreen();
         }
     }
@@ -216,36 +255,36 @@ void Calibration() {
     delay(10000);
     lcd.clear();
     lcdMillis = millis();
-    while(1) {
+    while (1) {
         currentMillis = millis();
         Screen2Items(StoreItems);
-        if(buttonCheck(BTN_ENTER_PIN, SHORT_PRESS)) {
+        if (buttonCheck(BTN_ENTER_PIN, SHORT_PRESS)) {
             break;
         }
-        if(buttonCheck(BTN_ESC_PIN, SHORT_PRESS)) {
+        if (buttonCheck(BTN_ESC_PIN, SHORT_PRESS)) {
             mainScreen();
         }
     }
     updateEEPROM();
+    getEEPROM();
 }
 
 void CalibrationScreen(int selectedNum[], int numberSize) {
     lcd.clear();
     lcd.setCursor(0,0);
     lcd.print("Input sample(g)..");
-    for(int i = 0; i < numberSize; i++){
+    for (int i = 0; i < numberSize; i++) {
         lcd.setCursor((15-i),1);
         lcd.print(selectedNum[i]);
     } 
 }
 
 void mainScreen() {
-    int selectedFunc = selection(MainScreenItems, 4);
+    int selectedFunc = selection(MainScreenItems, 3);
     switch (selectedFunc) {
         case 0:   Measure();           break;
         case 1:   Calibration();       break;
         // case 2:   Information();    break;
-        // case 3:   Advance();        break;
         default:                       break; //break or exit
     }
 }
